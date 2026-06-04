1be12dc4fc5453fbce2f0bed32f9ae872107a001
NiruIIT <nirupamcode@gmail.com>
2026-06-04 20:35:44 +0530

    Updated function, implemented object

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+let score = {
+    wins: 0,
+    losses: 0,
+    ties: 0,
+    displayScore: function(){
+        return `Won:${score.wins}, Lose:${score.losses}, Tie:${score.ties}`;
+    }
+};
+
 let computerChoice;
 function generateComputerChoice() {
     //This will generate random no between 0 and 3
@@ -14,18 +23,24 @@ function generateComputerChoice() {
 
 function getResult(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
+        score.ties++;
         return 'It\'s a tie!';
     } else if (
         (playerChoice === 'Bat' && computerChoice === 'Ball') ||
         (playerChoice === 'Ball' && computerChoice === 'Stump') ||
         (playerChoice === 'Stump' && computerChoice === 'Bat')
     ) {
+        score.wins++;
         return 'You win!';
     } else {
+        score.losses++;
         return 'You lose!';
     }
 }
 
 function showResult(userChoice, computerChoice, result) {
-    alert(`You chose ${userChoice}!\nComputer chose ${computerChoice}.\n${result}`);
+    alert(`You chose ${userChoice}!\nComputer chose ${computerChoice}.\n${result}
+        \n${score.displayScore()}`
+    );
+    
 }
